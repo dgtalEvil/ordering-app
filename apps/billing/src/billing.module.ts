4b29fc56d0be1986fe419b075fe167353a9d24fd
dgtalEvil <dgtalevil@gmail.com>
2024-05-19 20:43:45 +0530

    Integrated Auth with other services

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -2,7 +2,7 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
-import { RmqModule } from '@app/common';
+import { AuthModule, RmqModule } from '@app/common';
 import * as Joi from 'joi';
 
 @Module({
@@ -15,6 +15,7 @@ import * as Joi from 'joi';
       }),
     }),
     RmqModule,
+    AuthModule,
   ],
   controllers: [BillingController],
   providers: [BillingService],
